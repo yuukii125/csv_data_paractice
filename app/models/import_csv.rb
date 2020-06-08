f707ecfd1bba947c0ec1836c8c9029780cc308b4
yuukii125 <y.kipro125@gmail.com>
2020-06-08 22:33:41 +0900

    インポートを別の書き方で実装

--- app/models/import_csv.rb
+++ app/models/import_csv.rb
@@ -1,13 +1,36 @@
 class ImportCsv < ApplicationRecord
   #CSVデータのパスを引数として受け取り、インポート処理を実行
-  def self.import(path)
+  # def self.import(path)
+  #   CSV.foreach(path, headers: true) do |row|
+  #     User.create(
+  #       name: row["name"],         #CSVデータのname列の情報を指定して読み込み
+  #       age: row["age"],           #CSVデータのage列の情報を指定して読み込み
+  #       address: row["address"]   #CSVデータのaddress列の情報を指定して読み込み
+  #     )
+  #   end
+  # end
+
+  #改善version
+  def self.users_data
+    #インポートするファイルのパスを取得
+    path = File.join Rails.root, "db/csv_data/csv_data.csv"
+    #インポートするデータを格納するための配列
+    list = []
+    #CSVファイルからインポートするデータを取得し配列に格納
     CSV.foreach(path, headers: true) do |row|
-      User.create(
-        name: row["name"],         #CSVデータのname列の情報を指定して読み込み
-        age: row["age"],           #CSVデータのage列の情報を指定して読み込み
-        address: row["address"]   #CSVデータのaddress列の情報を指定して読み込み
-      )
+      list << {
+        name: row["name"],
+        age: row["age"],
+        address: row["address"]
+      }
     end
-  end
-  
+    puts "インポート処理を開始"
+    #インポートできなかった場合の例外処理
+    begin
+      User.create!(list)
+      puts "インポート完了"
+    rescue ActiveModel::UnknownAttributeError => invalid
+      puts "インポートに失敗：UnknownAttributeError"
+    end
+  end 
 end
